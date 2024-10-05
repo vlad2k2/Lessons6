303abbb924d59d50c7cddb9a675ce76206bce3e7
Vlad <vlad_rvz@ukr.net>
2024-10-06 02:17:39 +0300

    out of code

--- src/app/Lessons6Solutions.java
+++ src/app/Lessons6Solutions.java
@@ -2,9 +2,27 @@ package app;
 
 public class Lessons6Solutions {
 
+    static int numberInOrder = 1;
+
     public static void main(String[] args){
+        int sum = 0;
+        int i;
 
+        for(i = 1; i <= 6; i++){
+            sum = sum + i;
+            System.out.printf("%d) Num is %d, sum is %d %n", numberInOrder++, i, sum);
 
+        }
+        int a = sum;
+        while (true){
+            System.out.print("-");
+            a--;
+            if(a == 0){
+                break;
+            }
+        }
+        System.out.println();
+        System.out.printf("Sum of numbers is %d \n", sum);
 
     }
 
